f2bed1492f7d1477fecdca272ba75b7ac088d594
Ahmad Ali <ahmad.ali@kongsbergdigital.com>
2022-03-10 13:15:18 +0100

    - Added script to restart and run migrations and DB - Added authentication logic - Added authentication validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-
   constructor(private prisma: PrismaService) {}
-  login() {
-    return { msg: 'this is the login' };
-  }
+  login = async (dto: AuthDto) => {
+    // check if user exists
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new NotFoundException('No user found');
 
-  signup() {
-    return { msg: 'this is the signup' };
-  }
+    const passwordMatches = await argon.verify(user.hash, dto.password);
+
+    if (!passwordMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    delete user.hash;
+    return user;
+  };
+
+  signup = async (dto: AuthDto) => {
+    // Create a password hash using argo2
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // save the user to the database
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      // return the user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Credentials already taken');
+        }
+      }
+    }
+  };
 }
