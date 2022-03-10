43f63cf4e2557873458b9a47e271c724ade4cc1d
Ahmad Ali <ahmad.ali@kongsbergdigital.com>
2022-03-10 14:11:59 +0100

    - Added jwt and passport for authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,10 +7,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   login = async (dto: AuthDto) => {
     // check if user exists
     const user = await this.prisma.user.findUnique({
@@ -26,7 +32,7 @@ export class AuthService {
       throw new ForbiddenException('Credentials incorrect');
     }
     delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
   };
 
   signup = async (dto: AuthDto) => {
@@ -44,13 +50,33 @@ export class AuthService {
 
       delete user.hash;
       // return the user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code == 'P2002') {
           throw new ForbiddenException('Credentials already taken');
         }
       }
+      throw error;
     }
+  };
+
+  signToken = async (
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> => {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
   };
 }
